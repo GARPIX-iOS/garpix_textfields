cfa528ed3b99c105241690081aad81b778ed8782
WeDontNeedTo <danlomaev@yandex.ru>
2021-10-18 15:19:56 +0300

    fix paddinga

--- Sources/GXTextField/Modifiers/BorderTFStyle/BorderTFStyle.swift
+++ Sources/GXTextField/Modifiers/BorderTFStyle/BorderTFStyle.swift
@@ -18,6 +18,7 @@ public struct BorderTFStyle: ViewModifier {
     /// - Returns: TF с новым стилем
     public func body(content: Content) -> some View {
         content
+            .padding(.horizontal)
             .overlay(
                 ZStack(alignment: .topLeading) {
                     Group {
